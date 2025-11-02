4330f2e686354c55f1184ac575be0dacd0a630db
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-11-02 04:15:31 -0800

    Create a User Profile Card Using Props

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+const UserProfile = (props) => {
+const {name, age, bio} = props;
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Age : {age}</p>
+            <p>Bio : {bio}</p>
+        </div>
+    );
+}
+
+export default UserProfile;
