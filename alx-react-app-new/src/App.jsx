e39535c847138b0c21aa958e51710ee6949ef661
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-11-09 08:42:51 -0800

    A Simple Counter Application Using State and Hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
-
+import Counter from './components/Counter.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,6 +18,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
