5da6e9225dc8a88780e4560392f04c7a40fce76c
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-11-11 01:06:57 -0800

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <footer style={{color: 'purple', textAlign: 'center', marginTop: '20px'}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
