e1284e6e7a4928e7e1662eb6092b4add8c89d63e
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-11-21 23:16:34 -0800

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2 rem", fontFamily: "sans-serif"}}>
+      <h1>Github User Search</h1>
+      <p>Search for Github users by their user name</p>
+    </div>
+  );
+}
+
+export default App;
