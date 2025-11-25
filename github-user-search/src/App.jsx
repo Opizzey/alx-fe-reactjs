ab2a5c541d8969ae1161ac05fe3fe8f1271042ff
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-11-25 14:39:49 -0800

    Deploying the GitHub User Search Application to Vercel

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,7 +4,7 @@ import Search from "./components/Search";
 function App() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-white">
-      <h1 className="text-center font-bold text-2xl mt-6">Github User Search</h1>
+      <h1 className="text-center font-bold text-2xl mt-6">Github User Search.</h1>
       <p className="text-center text-gray-700">Search for Github users by their user name.</p>
       <Search />
     </div>
