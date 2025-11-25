fbc38395466892ae366a15767ec371d5ac467ea2
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-11-25 14:11:44 -0800

    Edited teh format on App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,9 +4,9 @@ import Search from "./components/Search";
 function App() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-white">
-      <Search />
       <h1 className="text-center font-bold text-2xl mt-6">Github User Search</h1>
       <p className="text-center text-gray-700">Search for Github users by their user name.</p>
+      <Search />
     </div>
   );
 }
